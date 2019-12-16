579733000d9c9a6e834f6dc0a12156c1eebd7ae7
S <55607380+pizza2u@users.noreply.github.com>
2019-12-16 10:00:46 -0300

    Update for.py

--- exemplos_basicos/python/for.py
+++ exemplos_basicos/python/for.py
@@ -1,6 +1,12 @@
-#i = [ 0,1,2,3,4,5,6]
-#for v in i:
-    #print(v) -> limitado (imprime  os números do vetor)
+#>>>>>>>>>>>>>>>> Código 1:
+i = [ 0,1,2,3,4,5,6]
+for v in i:
+    print(v) #-> limitado (imprime  os números do vetor)
+    
+    
+#------------------------------------------------------------------
 
+
+#>>>>>>>>>>>>>>> Código 2:
 for v in range(200): #-> vai de 0 ate 199
-    print(v)
+    print(v) #-> imprime de 0 ate 199

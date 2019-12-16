a2e2884a40cd4f5b6efacf3898a4b1505894b413
S <55607380+pizza2u@users.noreply.github.com>
2019-12-16 10:02:46 -0300

    Update while.py
    Repetição

--- exemplos_basicos/python/while.py
+++ exemplos_basicos/python/while.py
@@ -1,7 +1,14 @@
+# >>>>>>>>>Código 1: IMRPIME EM LINHA
 i = 0
 while i<= 20:
-    print(i,end = " ") # -> em linha
-    # em coluna -> print(i)
+    print(i,end = " ") 
     i+=1
 
+#--------------------------------------------------------
+
+# >>>>>>>>>>>>>> Código 2: IMPRIME EM COLUNA
+i = 0
+while i<= 20:
+    print(i)
+    i+=1
   

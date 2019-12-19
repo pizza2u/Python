f9ef8937a7ee92c08369a687a8679ef2ae4c63e3
S <55607380+pizza2u@users.noreply.github.com>
2019-12-19 16:29:50 -0300

    Add files via upload

---
+++ exemplos_basicos/python/WhileCOMOR.py
@@ -0,0 +1,23 @@
+numero =21
+tentativa = 3
+
+rodada = 1
+while(tentativa >= rodada):
+    print("*********tentativa {} de {}".format(rodada,tentativa))
+    chute = int(input("bora: "))
+    
+    if chute == numero:
+        print("acertou")
+        break
+    else:
+        if chute> numero:
+            print("o chute é maior que o numero")
+           
+        if chute < numero:
+            print(" o chute é menor")
+
+    rodada = rodada +1
+           
+
+
+print('fim de jogo')

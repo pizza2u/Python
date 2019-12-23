8ff3c73a8167bffda0c7fffe764f76157ee77e97
S <55607380+pizza2u@users.noreply.github.com>
2019-12-23 17:31:07 -0300

    Add files via upload

---
+++ exemplos_basicos/python/espacamento.py
@@ -0,0 +1,7 @@
+nome = input(" Fala bro : ")
+print(" Olá amigoa {:>20}!".format(nome)) # espaçamento de 20 caracters para direita
+print(" Olá amigoa {:<20}!".format(nome))# espaçamento de 20 caracters para esquerda
+print(" Olá amigoa {} !".format(nome)) #normal
+print(" Olá amigoa {:^20}!".format(nome)) #centralizado
+print(" Olá amigoa {:=^20}!".format(nome)) #nome + sinal de = para outros caracters
+print(" Olá amigoa {:2^20}!".format(nome)) # da mesma forma anterior so mudou o sinal

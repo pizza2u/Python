a5111e67f7618acc34cacf7c0172c0a6d8f1252d
S <55607380+pizza2u@users.noreply.github.com>
2019-12-23 16:31:14 -0300

    Add files via upload

--- exemplos_basicos/python/dados_e_contas/exemplo_tipo_bool.py
+++ exemplos_basicos/python/dados_e_contas/exemplo_tipo_bool.py
@@ -1,3 +1,22 @@
 n = bool(input("Digite algo: "))
 
-print(n)
+print(n) 
+# Lógica bool -> caso seja digitado algo o valor retorna como True, se nada for digitado o valor retorna False
+
+
+#--------------------------------------------------- Forma numerica
+n = input("Digite algo: ")
+print(n.isnumeric()) # .isnumeric representa uma pergunta do tipo "é numerico?" caso seja ele retorna True, caso não retorna False
+# também diz se é possivel converter o valor em um numero do tipo primitivo int antes dele
+
+#------------------------------------------------- Forma alpha
+n = input("Digite algo: ")
+print(n.isalpha()) #.isalpha() possui a mesma logica para um alfabetico
+
+#------------------------------------------------ Forma alpha numerico
+n = input("Digite algo: ")
+print(n.isalnum()) #isalnum()retorna True para alfabeticos e numericos, retorna False em por exemplo ' '
+
+#---------------------------------------------- Forma is upper
+n = input("Digite algo: ")
+print(n.isupper()) #isupper é uma função para retornar True casos as palavras ou letras escritas estiverem em maiusculo e False caso alguma esteja em minusculo.

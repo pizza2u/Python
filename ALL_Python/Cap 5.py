badb3a10815a764d2850acc4440f914d84554a5b
Ant <55607380+pizza2u@users.noreply.github.com>
2021-06-12 19:34:05 -0300

    Update Cap 5.py

--- ALL_Python/Cap 5.py
+++ ALL_Python/Cap 5.py
@@ -20,7 +20,7 @@ while cont <= 100:
        print(cont)
        cont += 1
        # O que que rolou nesse código? Então, nesse código o laço não vai se repetir para sempre, digamos que ele tenha um critério de parada, e esse critério é 100, a cada vez que o código se repetir , a variável cont vai ter um valor diferente
-       # ou seja para cada cont rodado, o valor vai recever +1.
+       # ou seja para cada cont rodado, o valor vai receber +1.
        # Mas quando o valor de cont for maior que 100 o programa vai parar
 
 # LAÇO FOR

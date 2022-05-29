31c8f4a42a2abbf06bec4489c0585381f657a54f
Spock <55607380+pizza2u@users.noreply.github.com>
2022-05-29 13:35:41 -0300

    Rename outro.py to datas.py

---
+++ exemplos_basicos/python/datas.py
@@ -0,0 +1,8 @@
+
+dia = int(input(""))
+mes =int(input(""))
+ano = int(input(""))
+uno= 20
+
+
+print("O carnaval acontece dia {:02.0f}/{:02.0f}/{}" .format(dia,mes, uno + ano)) #-> para printar os unicas duas casas digitos com-> :02.0f 

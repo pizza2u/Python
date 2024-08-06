c73310d700dd6bd58f6d8e6a37d294c21b3c29e3
sara.nobrega <luvstaeil@gmail.com>
2024-08-06 19:00:26 -0300

    att

--- Bibliotecas/Biblioteca Selenium/1.py
+++ Bibliotecas/Biblioteca Selenium/1.py
@@ -1,1 +1,28 @@
-#soon
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Caminho para o WebDriver (substitua pelo caminho correto do seu WebDriver)
+driver_path = 'path/to/chromedriver'
+
+# Inicializa o navegador
+driver = webdriver.Chrome(executable_path=driver_path)
+
+# Abre a página desejada
+driver.get('https://www.python.org')
+
+try:
+    # Espera até que um elemento específico esteja presente na página (tempo máximo de espera de 10 segundos)
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'element_id'))
+    )
+    # Interaja com o elemento encontrado
+    element.click()
+
+except:
+    print("Elemento não encontrado no tempo especificado")
+
+finally:
+    # Fecha o navegador
+    driver.quit()
